5fa00fd5c62bbc4bac69f9659e8dfb44a49cfe29
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-09-28 18:31:25 -0300

    added localhost mapping

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,8 @@ config :gym_system_api, GymSystemApi.Repo,
 config :gym_system_api, GymSystemApiWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [port: 4000],
+  url: [host: "localhost"],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
