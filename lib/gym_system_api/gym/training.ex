76ae71396603b9f5a9c4c6b41383a787003f9283
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-10-01 18:58:21 -0300

    added traning module

---
+++ lib/gym_system_api/gym/training.ex
@@ -0,0 +1,22 @@
+defmodule GymSystemApi.Gym.Training do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias GymSystemApi.Gym.Bodybuilder
+
+  schema "trainings" do
+    field :description, :string
+    field :exercises, {:array, :string}
+
+    belongs_to(:bodybuilder, Bodybuilder)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(trainings, attrs) do
+    trainings
+    |> cast(attrs, [:description, :exercises, :bodybuilder_id])
+    |> validate_required([:description])
+  end
+end
