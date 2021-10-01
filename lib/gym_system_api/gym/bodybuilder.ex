4fb381ef5e5cf1ab10c2f91418b99398526ce846
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-10-01 18:57:50 -0300

    renamed bodybuilder

--- lib/gym_system_api/gym/bodybuilder.ex
+++ lib/gym_system_api/gym/bodybuilder.ex
@@ -1,17 +1,21 @@
-defmodule GymSystemApi.Gym.Bodybuilders do
+defmodule GymSystemApi.Gym.Bodybuilder do
   use Ecto.Schema
   import Ecto.Changeset
+
+  alias GymSystemApi.Gym.Training
 
   schema "body_builders" do
     field :cpf, :string
     field :name, :string
 
+    has_many(:trainings, Training, on_delete: :delete_all)
+
     timestamps()
   end
 
   @doc false
-  def changeset(bodybuilders, attrs) do
-    bodybuilders
+  def changeset(bodybuilder, attrs) do
+    bodybuilder
     |> cast(attrs, [:name, :cpf])
     |> validate_required([:name, :cpf])
   end
