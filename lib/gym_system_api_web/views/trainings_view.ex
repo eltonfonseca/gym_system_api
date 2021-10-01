4c00cbef08730232da297b2fed94aff88500bc79
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2021-10-01 18:59:56 -0300

    added training view

---
+++ lib/gym_system_api_web/views/trainings_view.ex
@@ -0,0 +1,20 @@
+defmodule GymSystemApiWeb.TrainingsView do
+  use GymSystemApiWeb, :view
+  alias GymSystemApiWeb.TrainingsView
+
+  def render("index.json", %{trainings: trainings}) do
+    %{data: render_many(trainings, TrainingsView, "training.json", as: :training)}
+  end
+
+  def render("training.json", %{training: training}) do
+    %{
+      id: training.id,
+      description: training.description,
+      exercises: training.exercises
+    }
+  end
+
+  def render("404.json", %{message: message}) do
+    %{error: message}
+  end
+end
